c87cbc74366c72f7c9e9ab9b5fcc43183877a350
your_username <asawalov@gmail.com>
2021-02-02 13:36:49 +0530

    Blogwebsite

---
+++ iCoder/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key = "True")
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=14)
+    
+    content = models.TextField()
+
+    def __str__(self):
+        return "Message from " + self.name + " email_id is : " + self.email
+
